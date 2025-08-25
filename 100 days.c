f9f498bc7068fbfe87c91f3223e9617b99438e78
anushkashukla5281-cpu <anushkashukla5281@gmail.com>
2025-08-25 15:11:28 +0530

    Add files via upload

---
+++ 100 days.c
@@ -0,0 +1,11 @@
+#include<stdio.h>
+int main()
+{
+ float a,b,sum;
+ printf("please enter the first number - ");
+ scanf("%f" , &a);
+ printf("please enter the second number - ");
+ scanf("%f" , &b);
+ sum = a+b ;
+ printf("the sum is %f" , sum);
+}
